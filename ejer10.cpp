14a19416e2b30cf04ca8bcf81b186901c44bdd54
edson <edsonaranda25@gmail.com>
2015-12-01 07:43:57 -0500

    Ejercicio 10

---
+++ ejer10.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+using namespace std;
+
+int main()
+{
+     int a, x,y;
+     double tmp1=6.25;
+     double tmp2=11.25;
+     a=tmp1;
+     x=a+tmp2;
+     y=a*tmp1;
+     cout<<"Tmp1 es igual a: "<<tmp1<<endl;
+     cout<<"X es igual a: "<<x<<endl;
+     cout<<"Y es igual a: "<<y<<endl;
+     return 0;
+}
